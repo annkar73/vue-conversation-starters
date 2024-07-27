6fca92261561bd3fe7e521c08b861aed45b90b3b
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-07-27 21:00:07 +0200

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/conversation-starters/',
+  base: '/vue-conversation-starters/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
